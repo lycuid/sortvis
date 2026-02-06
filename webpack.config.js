f5138badbf93664c0eab491583328e7356405949
Abhishek Kadam <abhishekk19@gmail.com>
2026-02-06 14:43:05 +0530

    [Refactor]: version bumps and build config optimizations.

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,57 @@
 const Package = require('./package');
 const { resolve } = require('path');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const src = (p) => resolve(__dirname, 'src', p);
 
 module.exports = (env) => ({
-  entry: [src('index.ts'), src('style.css')],
+  mode: env.production ? 'production' : 'development',
+
+  entry: {
+    main: [src('index.ts'), src('style.css')]
+  },
+
   output: {
     path: resolve(__dirname, 'public'),
-    publicPath: env.production ? `https://cdn.lycuid.dev/p/${Package.name}` : '/',
-    filename: env.production ? '[name].[contenthash].js' : '[name].js'
+    publicPath: env.production ? `https://cdn.lycuid.dev/p/${Package.name}/` : '/',
+    filename: env.production ? '[name].[contenthash].js' : '[name].js',
+    clean: true, // clean old assets in output dir
   },
+
   module: {
     rules: [
       {
-        exclude: /node_modules/,
         test: /\.ts$/,
-        loader: 'awesome-typescript-loader'
+        exclude: /node_modules/,
+        use: 'ts-loader' // Replacing deprecated awesome-typescript-loader
       },
       {
-        exclude: /node_modules/,
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
+      }
     ]
   },
+
   plugins: [
     new MiniCssExtractPlugin({
       filename: env.production ? 'style.[contenthash].css' : 'style.css'
     }),
     new CssMinimizerPlugin(),
-    new HTMLWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: src('index.html'),
-      inject: true
-    }),
+      inject: 'body'
+    })
   ],
-  resolve: { extensions: ['.js', '.ts'] }
-});
 
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  optimization: {
+    minimize: env.production,
+    minimizer: ['...', new CssMinimizerPlugin()]
+  },
+});
